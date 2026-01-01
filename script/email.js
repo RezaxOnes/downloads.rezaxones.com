1c312bd0d6fcac14ac6f14f9d80173c4e5a4fabf
rezaxones <rezaxoneslimited1@gmail.com>
2026-01-01 19:53:09 +0700

    fix

--- script/email.js
+++ script/email.js
@@ -1,6 +1,4 @@
-
-  const user = "rezaxonesassist";
-  const domain = "gmail.com";
-  const email = user + "@" + domain;
-  const container = document.getElementById("My_Email");
-  container.innerHTML = `<a href="mailto:${email}" style="color:#000000;">${email}</a>`;
+const user = "rezaxonesassist";
+const domain = "gmail.com";
+const container = document.getElementById("My_Email");
+container.innerHTML = `<a href="mailto:${user}@${domain}" style="color:#000000;">${user}@${domain}</a>`;
